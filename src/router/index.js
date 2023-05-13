22e35c7aaa4e4b3824b0d7f41208e64b1d473d3e
Kakayou <shigaev_1990@mail.ru>
2023-05-13 18:58:48 +0700

    Жеско расписаны пути

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import AddDoctor from "@/pages/AddDoctor.vue";
 import TimetablePage from "@/pages/TimetablePage.vue";
+import Appointment from "@/pages/Appointment.vue";
+import Control from "@/pages/Control.vue";
+import DiagnosisAdd from "@/pages/DiagnosisAdd.vue";
+import HealAdd from "@/pages/HealAdd.vue";
+import Login from "@/pages/Login.vue";
+import PatientHistory from "@/pages/PatientHistory.vue";
+import Patients from "@/pages/Patients.vue";
+import RecordPatient from "@/pages/RecordPatient.vue";
 
 const routes = [
     {
@@ -10,6 +18,38 @@ const routes = [
     {
         path: '/add-doctor',
         component: AddDoctor
+    },
+    {
+        path: '/appointment',
+        component: Appointment
+    },
+    {
+        path: '/control',
+        component: Control
+    },
+    {
+        path: '/diagnosis-add',
+        component: DiagnosisAdd
+    },
+    {
+        path: '/heal-add',
+        component: HealAdd
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/patient-history',
+        component: PatientHistory
+    },
+    {
+        path: '/patients',
+        component: Patients
+    },
+    {
+        path: '/record-patient',
+        component: RecordPatient
     },
     // {
     //     path: '*',
